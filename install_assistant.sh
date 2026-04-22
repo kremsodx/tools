f817cefd6bc0b905e4de3895b11c804cb13135b8
kremsodx <kremsodx@gmail.com>
2026-04-22 12:45:00 +0700

    Update install_assistant.sh

--- install_assistant.sh
+++ install_assistant.sh
@@ -9,6 +9,20 @@ fi
 echo "--- Отключение UFW ---"
 ufw disable
 
+echo "--- Отключение ЗПС ---"
+astra-digsig-control disable
+
+echo "--- Замена на сетевые репозитории ---"
+bash -c 'cat << EOF > /etc/apt/sources.list
+deb https://download.astralinux.ru/astra/stable/1.7_x86-64/repository-main/ 1.7_x86-64 main contrib non-free
+deb https://download.astralinux.ru/astra/stable/1.7_x86-64/repository-update/ 1.7_x86-64 main contrib non-free
+deb https://download.astralinux.ru/astra/stable/1.7_x86-64/repository-base/ 1.7_x86-64 main contrib non-free
+deb https://download.astralinux.ru/astra/stable/1.7_x86-64/repository-extended/ 1.7_x86-64 main contrib non-free
+EOF'
+
+echo "--- Получение списка пакетов ---"
+apt update
+
 echo "--- Скачивание ассистента ---"
 # Используем -O для сохранения файла под понятным именем
 wget -O assistant.deb https://lk2.мойассистент.рф/WebApi/Platforms/Download/1375
@@ -18,3 +32,15 @@ echo "--- Установка пакета ---"
 apt install ./assistant.deb -y
 
 echo "--- Готово! ---"
+
+seconds=10
+echo "Внимание! Компьютер перезагрузится через $seconds секунд."
+
+while [ $seconds -gt 0 ]; do
+    echo -ne "Осталось: $seconds сек. \r"
+    sleep 1
+    : $((seconds--))
+done
+
+echo -e "\nВыполняю перезагрузку..."
+reboot now
